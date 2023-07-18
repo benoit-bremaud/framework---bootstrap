0440e1bf1048756b3b1a7421564c4adcd3a60055
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-18 13:41:35 +0200

    feat: Add responsive grid layout using Bootstrap

--- tutoriel/index.php
+++ tutoriel/index.php
@@ -4,45 +4,35 @@
     <!-- Required meta tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Le système de grille de Boostrap</title>
+    <title>La grille responsive de Boostrap</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col border">1</div>
-            <div class="col border">2</div>
-            <div class="col border">3</div>
-            <div class="col border">4</div>
-            <div class="col border">5</div>
-            <div class="col border">6</div>
-            <div class="col border">7</div>
-            <div class="col border">8</div>
-            <div class="col border">9</div>
-            <div class="col border">10</div>
-            <div class="col border">11</div>
-            <div class="col border">12</div>
+   <div class="container">
+    <div class="row">
+        <div class="col-md-6 col-lg-3">
+            <h2>Colonne 1</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero asperiores cum beatae repellendus, odit at amet dolorum optio accusantium doloribus dolores animi, quaerat nihil deleniti delectus iure voluptatem reprehenderit quas?
+            </p>
         </div>
-        <div class="row">
-            <div class="col border">A</div>
-            <div class="col border">B</div>
+        <div class="col-md-6 col-lg-3">
+            <h2>Colonne 2</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero asperiores cum beatae repellendus, odit at amet dolorum optio accusantium doloribus dolores animi, quaerat nihil deleniti delectus iure voluptatem reprehenderit quas?
+            </p>
         </div>
-        <div class="row">
-            <div class="col-4 border">AA</div>
-            <div class="col-8 border">BB</div>
+        <div class="col-md-6 col-lg-3">
+            <h2>Colonne 3</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero asperiores cum beatae repellendus, odit at amet dolorum optio accusantium doloribus dolores animi, quaerat nihil deleniti delectus iure voluptatem reprehenderit quas?
+            </p>
         </div>
-        <div class="row">
-            <div class="col-1 border">A</div>
-            <div class="col-2 border">B</div>
-            <div class="col-3 border">C</div>
-            <div class="col-6 border">D</div>
-            <div class="col-4 border">E</div>
-            <div class="col-4 border">F</div>
-            <div class="col-4 border">G</div>
+        <div class="col-md-6 col-lg-3">
+            <h2>Colonne 4</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero asperiores cum beatae repellendus, odit at amet dolorum optio accusantium doloribus dolores animi, quaerat nihil deleniti delectus iure voluptatem reprehenderit quas?
+            </p>
         </div>
     </div>
-
+   </div>
 </body>
 </html>
