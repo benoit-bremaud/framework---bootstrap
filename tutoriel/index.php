d6021f550a8408008e89eb79206116a2053a4e06
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-18 13:48:59 +0200

    feat: Implement alternative responsive grid layout

--- tutoriel/index.php
+++ tutoriel/index.php
@@ -4,35 +4,35 @@
     <!-- Required meta tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>La grille responsive de Boostrap</title>
+    <title>Autre méthode de grille responsive</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
    <div class="container">
-    <div class="row">
-        <div class="col-md-6 col-lg-3">
-            <h2>Colonne 1</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero asperiores cum beatae repellendus, odit at amet dolorum optio accusantium doloribus dolores animi, quaerat nihil deleniti delectus iure voluptatem reprehenderit quas?
-            </p>
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4">
+            <div class="col">
+                <h2>Colonne 1</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero asperiores cum beatae repellendus, odit at amet dolorum optio accusantium doloribus dolores animi, quaerat nihil deleniti delectus iure voluptatem reprehenderit quas?
+                </p>
+            </div>
+            <div class="col">
+                <h2>Colonne 2</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero asperiores cum beatae repellendus, odit at amet dolorum optio accusantium doloribus dolores animi, quaerat nihil deleniti delectus iure voluptatem reprehenderit quas?
+                </p>
+            </div>
+            <div class="col">
+                <h2>Colonne 3</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero asperiores cum beatae repellendus, odit at amet dolorum optio accusantium doloribus dolores animi, quaerat nihil deleniti delectus iure voluptatem reprehenderit quas?
+                </p>
+            </div>
+            <div class="col">
+                <h2>Colonne 4</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero asperiores cum beatae repellendus, odit at amet dolorum optio accusantium doloribus dolores animi, quaerat nihil deleniti delectus iure voluptatem reprehenderit quas?
+                </p>
+            </div>
         </div>
-        <div class="col-md-6 col-lg-3">
-            <h2>Colonne 2</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero asperiores cum beatae repellendus, odit at amet dolorum optio accusantium doloribus dolores animi, quaerat nihil deleniti delectus iure voluptatem reprehenderit quas?
-            </p>
-        </div>
-        <div class="col-md-6 col-lg-3">
-            <h2>Colonne 3</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero asperiores cum beatae repellendus, odit at amet dolorum optio accusantium doloribus dolores animi, quaerat nihil deleniti delectus iure voluptatem reprehenderit quas?
-            </p>
-        </div>
-        <div class="col-md-6 col-lg-3">
-            <h2>Colonne 4</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero asperiores cum beatae repellendus, odit at amet dolorum optio accusantium doloribus dolores animi, quaerat nihil deleniti delectus iure voluptatem reprehenderit quas?
-            </p>
-        </div>
-    </div>
    </div>
 </body>
 </html>
