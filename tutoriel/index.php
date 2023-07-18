7cab275d66def6755afa7946328ee48279ef1f54
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-18 12:04:33 +0200

    feat: Add container class

--- tutoriel/index.php
+++ tutoriel/index.php
@@ -4,23 +4,28 @@
     <!-- Required meta tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Installation de bootstrap</title>
+    <title>La classe CONTAINER</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Installation de bootstrap</h1>
+    <div class="container-fluid">
+        <h1>La classe CONTAINER</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis suscipit at deleniti sapiente alias consectetur architecto dolore, illo totam voluptatem, illum ullam molestias voluptatibus facere explicabo expedita aut, optio laudantium possimus iusto non! Accusamus error, praesentium labore ipsam sit dolorum. Voluptatibus repellat atque molestias sapiente praesentium dolores repudiandae facere doloribus temporibus culpa deleniti aut autem exercitationem magnam aperiam similique aliquid, corporis a cum saepe aspernatur! Cupiditate accusantium sit, nisi libero repellendus numquam illum iusto veritatis fuga quis in provident eligendi obcaecati aliquam repellat sunt nihil. Id aperiam laudantium voluptates repellat temporibus eaque itaque eligendi? Quod impedit obcaecati rem a exercitationem.
+        </p>
+
+    </div>
     
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> -->
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     -->
-    
+
 </body>
 </html>
