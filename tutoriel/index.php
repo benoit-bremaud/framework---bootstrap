d84f4a37ce63c169d1dde2b6545f4460aef7e0bb
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-18 12:22:02 +0200

    feat: Add Bootstrap grid system presentation

--- tutoriel/index.php
+++ tutoriel/index.php
@@ -4,28 +4,45 @@
     <!-- Required meta tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>La classe CONTAINER</title>
+    <title>Le système de grille de Boostrap</title>
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container-fluid">
-        <h1>La classe CONTAINER</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis suscipit at deleniti sapiente alias consectetur architecto dolore, illo totam voluptatem, illum ullam molestias voluptatibus facere explicabo expedita aut, optio laudantium possimus iusto non! Accusamus error, praesentium labore ipsam sit dolorum. Voluptatibus repellat atque molestias sapiente praesentium dolores repudiandae facere doloribus temporibus culpa deleniti aut autem exercitationem magnam aperiam similique aliquid, corporis a cum saepe aspernatur! Cupiditate accusantium sit, nisi libero repellendus numquam illum iusto veritatis fuga quis in provident eligendi obcaecati aliquam repellat sunt nihil. Id aperiam laudantium voluptates repellat temporibus eaque itaque eligendi? Quod impedit obcaecati rem a exercitationem.
-        </p>
-
+    <div class="container">
+        <div class="row">
+            <div class="col border">1</div>
+            <div class="col border">2</div>
+            <div class="col border">3</div>
+            <div class="col border">4</div>
+            <div class="col border">5</div>
+            <div class="col border">6</div>
+            <div class="col border">7</div>
+            <div class="col border">8</div>
+            <div class="col border">9</div>
+            <div class="col border">10</div>
+            <div class="col border">11</div>
+            <div class="col border">12</div>
+        </div>
+        <div class="row">
+            <div class="col border">A</div>
+            <div class="col border">B</div>
+        </div>
+        <div class="row">
+            <div class="col-4 border">AA</div>
+            <div class="col-8 border">BB</div>
+        </div>
+        <div class="row">
+            <div class="col-1 border">A</div>
+            <div class="col-2 border">B</div>
+            <div class="col-3 border">C</div>
+            <div class="col-6 border">D</div>
+            <div class="col-4 border">E</div>
+            <div class="col-4 border">F</div>
+            <div class="col-4 border">G</div>
+        </div>
     </div>
-    
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> -->
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    -->
 
 </body>
 </html>
