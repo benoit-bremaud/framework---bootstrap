652e99921eb382e48bab5d4b397dfed9bb48bb6a
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-18 16:38:50 +0200

    feat: added utility classes for margin and padding in Bootstrap
    Implemented utility classes to control margin and padding in Bootstrap. The code includes paragraphs with different margin and padding classes applied. The classes used include mt-5 (margin-top), ms-3 (margin-start), me-4 (margin-end), my-2 (vertical margin), mx-4 (horizontal margin), pb-5 (padding-bottom), and ps-5 (padding-start).
    
    Refs: #123

--- tutoriel/index.php
+++ tutoriel/index.php
@@ -4,27 +4,13 @@
     <!-- Required meta tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Les classes utilitaires : background color</title>
+    <title>Les classes utilitaires : les marges internes et externes</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container">
-        <p class="bg-gradient bg-primary text-white">bg-primary</p>
-        <p class="bg-gradient bg-secondary text-white">bg-secondary</p>
-        <p class="bg-gradient bg-success text-white">bg-success</p>
-        <p class="bg-gradient bg-danger text-white">bg-danger</p>
-        <p class="bg-gradient bg-warning text-white">bg-warning</p>
-        <p class="bg-gradient bg-info text-white">bg-info</p>
-        <p class="bg-gradient bg-light text-primary">bg-light</p>
-    </div>
-    <div class="container">
-        <p class="bg-danger">Opacité à 100%</p>
-        <p class="bg-danger bg-opacity-75">Opacité à 75%</p>
-        <p class="bg-danger bg-opacity-50">Opacité à 50%</p>
-        <p class="bg-danger bg-opacity-25">Opacité à 25%</p>
-        <p class="bg-danger bg-opacity-10">Opacité à 10%</p>
-    </div>
+    <p class="bg-primary mt-5 ms-3 me-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quidem excepturi recusandae est porro veniam dignissimos earum saepe autem iusto itaque, vitae ipsam esse, asperiores neque cumque corporis voluptates enim!</p>
+    <p class="bg-secondary my-2 mx-4 pb-5 ps-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quidem excepturi recusandae est porro veniam dignissimos earum saepe autem iusto itaque, vitae ipsam esse, asperiores neque cumque corporis voluptates enim!</p>
 </body>
 </html>
