a9ff15162b2c124d382445420993d8d08873d467
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-18 15:56:49 +0200

    feat: added images with Bootstrap
    Added HTML tags and Bootstrap classes to display images using Bootstrap. Two images are displayed side by side within a container. The images have the "img-fluid" class to ensure they scale properly, and the "rounded" class to give them rounded corners.

--- tutoriel/index.php
+++ tutoriel/index.php
@@ -4,40 +4,19 @@
     <!-- Required meta tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemples de typographie avec Bootstrap</title>
+    <title>Les images avec bootstrap</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container text-center"> <!-- Centre tous les élements sur la page -->
-        <h1 class="display-1">Balise H1</h1>
-        <h1 class="h4">Balise H1</h1>
-        <h2>Balise H2</h2>
-        <h3>Balise H3</h3>
-        <h4>Balise H4</h4>
-        <h5>Balise H5</h5>
-        <h6>Balise H6</h6>
-        <h6 class="h1">Balise H6</h6>
-        <p class="display-2">Mettre en évidence du texte</p>
-
-        <h2 class="text-start">Liste à puces</h2> <!-- Justifier à gauche -->
-            <ul class="list-unstyled"> <!-- Permet de supprimer les puces -->
-                <li class="list-inline-item">Element 1</li>
-                <li class="list-inline-item">Element 2</li>
-                <li class="list-inline-item">Element 3</li>
-                <li class="list-inline-item">Element 4</li>
-                <li class="list-inline-item">Element 5</li>
-            </ul>
-        <h2 class="text-end">Liste ordonnée</h2> <!-- Justifier à droite -->
-            <ol class="list-unstyled"> <!-- Permet de supprimer les numéros -->
-                <li>Element 1</li>
-                <li>Element 2</li>
-                <li>Element 3</li>
-                <li>Element 4</li>
-                <li>Element 5</li>
-            </ol>
+    <div class="container">
+        <div class="w-25 float-start">
+            <img class="img-fluid rounded" src="images/image_1.jpeg" alt=""> <!-- img-thumbnail -->
+        </div>
+        <div class="w-25 float-end">
+            <img class="img-fluid rounded" src="images/image_2.jpeg" alt=""> <!-- img-thumbnail -->
+        </div>
     </div>
-
 </body>
 </html>
