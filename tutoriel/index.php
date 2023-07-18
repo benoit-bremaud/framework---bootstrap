70e68d9ad894c6cf53bd1d117ed11cd43e41d2aa
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-18 15:45:59 +0200

    feat: added examples of typography with Bootstrap
    Added HTML tags and Bootstrap classes to display various typography elements. The h1 to h6 tags are used for headings of different sizes, the p tag is used to highlight text, and the ul and ol tags are used for unordered and ordered lists, respectively. Bootstrap classes are used to apply formatting styles.

--- tutoriel/index.php
+++ tutoriel/index.php
@@ -4,35 +4,40 @@
     <!-- Required meta tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Autre méthode de grille responsive</title>
+    <title>Exemples de typographie avec Bootstrap</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-   <div class="container">
-        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4">
-            <div class="col">
-                <h2>Colonne 1</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero asperiores cum beatae repellendus, odit at amet dolorum optio accusantium doloribus dolores animi, quaerat nihil deleniti delectus iure voluptatem reprehenderit quas?
-                </p>
-            </div>
-            <div class="col">
-                <h2>Colonne 2</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero asperiores cum beatae repellendus, odit at amet dolorum optio accusantium doloribus dolores animi, quaerat nihil deleniti delectus iure voluptatem reprehenderit quas?
-                </p>
-            </div>
-            <div class="col">
-                <h2>Colonne 3</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero asperiores cum beatae repellendus, odit at amet dolorum optio accusantium doloribus dolores animi, quaerat nihil deleniti delectus iure voluptatem reprehenderit quas?
-                </p>
-            </div>
-            <div class="col">
-                <h2>Colonne 4</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero asperiores cum beatae repellendus, odit at amet dolorum optio accusantium doloribus dolores animi, quaerat nihil deleniti delectus iure voluptatem reprehenderit quas?
-                </p>
-            </div>
-        </div>
-   </div>
+    <div class="container text-center"> <!-- Centre tous les élements sur la page -->
+        <h1 class="display-1">Balise H1</h1>
+        <h1 class="h4">Balise H1</h1>
+        <h2>Balise H2</h2>
+        <h3>Balise H3</h3>
+        <h4>Balise H4</h4>
+        <h5>Balise H5</h5>
+        <h6>Balise H6</h6>
+        <h6 class="h1">Balise H6</h6>
+        <p class="display-2">Mettre en évidence du texte</p>
+
+        <h2 class="text-start">Liste à puces</h2> <!-- Justifier à gauche -->
+            <ul class="list-unstyled"> <!-- Permet de supprimer les puces -->
+                <li class="list-inline-item">Element 1</li>
+                <li class="list-inline-item">Element 2</li>
+                <li class="list-inline-item">Element 3</li>
+                <li class="list-inline-item">Element 4</li>
+                <li class="list-inline-item">Element 5</li>
+            </ul>
+        <h2 class="text-end">Liste ordonnée</h2> <!-- Justifier à droite -->
+            <ol class="list-unstyled"> <!-- Permet de supprimer les numéros -->
+                <li>Element 1</li>
+                <li>Element 2</li>
+                <li>Element 3</li>
+                <li>Element 4</li>
+                <li>Element 5</li>
+            </ol>
+    </div>
+
 </body>
 </html>
