42c48c459354970eabaf4ec8558a08e721989fda
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-18 16:57:55 +0200

    feat: added utility classes for borders in Bootstrap
    Implemented utility classes to apply borders to elements in Bootstrap. The code includes two paragraphs with different border classes applied. The first paragraph has a basic border of 2 pixels, while the second paragraph has a primary-colored border with a width of 2 pixels and a rounded border radius of 3 pixels.
    
    Refs: #123

--- tutoriel/index.php
+++ tutoriel/index.php
@@ -4,13 +4,23 @@
     <!-- Required meta tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Les classes utilitaires : les marges internes et externes</title>
+    <title>Les classes utilitaires : les bordures</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-    <p class="bg-primary mt-5 ms-3 me-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quidem excepturi recusandae est porro veniam dignissimos earum saepe autem iusto itaque, vitae ipsam esse, asperiores neque cumque corporis voluptates enim!</p>
-    <p class="bg-secondary my-2 mx-4 pb-5 ps-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quidem excepturi recusandae est porro veniam dignissimos earum saepe autem iusto itaque, vitae ipsam esse, asperiores neque cumque corporis voluptates enim!</p>
+    <div class="container">
+        <div class="row py-3"> <!-- Padding Top and Bottom of 3 -->
+            <div class="col">
+                <!-- Ajout d'une bordure sur le premier paragraphe -->
+                <p class="border border-2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos quas libero et modi aspernatur est repudiandae assumenda. Quod at consequuntur voluptatibus hic aspernatur aut ad provident magni exercitationem nesciunt a tenetur ducimus ullam, harum nobis impedit! Beatae magni omnis necessitatibus incidunt mollitia deserunt iusto asperiores labore rerum deleniti! Laborum, pariatur!</p>
+            </div>
+            <div class="col">
+                <!-- AUtre bordure sur paragraphe 2 -->
+                <p class="border border-primary border-2 rounded-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores harum optio debitis dolorem repellendus nisi eos. Incidunt nobis enim totam nesciunt! Pariatur blanditiis cupiditate nihil, consectetur ipsa explicabo voluptas alias repellat omnis nam ea dolorum laborum iure harum a, asperiores porro unde, ut esse corporis maxime suscipit! Eligendi, rem tempore?</p>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
