d7de5c7cc6642cd15d4fb11221d46a6bdc9b2b29
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-18 16:31:47 +0200

    feat: added utility classes for background color in Bootstrap
    Implemented utility classes to apply different background colors in Bootstrap. The code includes paragraphs with various background colors, such as primary, secondary, success, danger, warning, info, and light. Additionally, opacity classes are used to adjust the opacity level of the background color.
    
    Refs: #123

--- tutoriel/index.php
+++ tutoriel/index.php
@@ -4,43 +4,27 @@
     <!-- Required meta tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Les tableaux avec bootstrap</title>
+    <title>Les classes utilitaires : background color</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
     <div class="container">
-        <table class="table table-hover"> <!-- table-succes | table-primary | table-secondary -->
-            <thead>
-                <tr>
-                    <th>Prénom</th>
-                    <th>Nom</th>
-                    <th>Age</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Benoit</td>
-                    <td>Bremaud</td>
-                    <td>42</td>
-                </tr>
-            </tbody>
-            <tbody>
-                <tr> <!-- <tr class="table-primary" > -->
-                    <td>Viktor</td>
-                    <td>Argirov</td>
-                    <td>23</td>
-                </tr>
-            </tbody>
-            <tbody>
-                <tr> <!-- <tr class="table-danger"> -->
-                    <td>Valeria</td>
-                    <td>Pallas</td>
-                    <td>38</td>
-                </tr>
-            </tbody>
-        </table>
+        <p class="bg-gradient bg-primary text-white">bg-primary</p>
+        <p class="bg-gradient bg-secondary text-white">bg-secondary</p>
+        <p class="bg-gradient bg-success text-white">bg-success</p>
+        <p class="bg-gradient bg-danger text-white">bg-danger</p>
+        <p class="bg-gradient bg-warning text-white">bg-warning</p>
+        <p class="bg-gradient bg-info text-white">bg-info</p>
+        <p class="bg-gradient bg-light text-primary">bg-light</p>
+    </div>
+    <div class="container">
+        <p class="bg-danger">Opacité à 100%</p>
+        <p class="bg-danger bg-opacity-75">Opacité à 75%</p>
+        <p class="bg-danger bg-opacity-50">Opacité à 50%</p>
+        <p class="bg-danger bg-opacity-25">Opacité à 25%</p>
+        <p class="bg-danger bg-opacity-10">Opacité à 10%</p>
     </div>
 </body>
 </html>
