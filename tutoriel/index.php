29940f63050ecf94c8df6eb96f28179e1c746cbe
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-18 16:10:10 +0200

    feat: added table management with Bootstrap
    Implemented a table using Bootstrap's table classes to display data. The table has a responsive layout with hover effect. It includes a table header with three columns: "First Name," "Last Name," and "Age." The table body contains rows with sample data, showcasing different individuals and their respective information.
    
    Refs: #123

--- tutoriel/index.php
+++ tutoriel/index.php
@@ -4,19 +4,43 @@
     <!-- Required meta tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Les images avec bootstrap</title>
+    <title>Les tableaux avec bootstrap</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
     <div class="container">
-        <div class="w-25 float-start">
-            <img class="img-fluid rounded" src="images/image_1.jpeg" alt=""> <!-- img-thumbnail -->
-        </div>
-        <div class="w-25 float-end">
-            <img class="img-fluid rounded" src="images/image_2.jpeg" alt=""> <!-- img-thumbnail -->
-        </div>
+        <table class="table table-hover"> <!-- table-succes | table-primary | table-secondary -->
+            <thead>
+                <tr>
+                    <th>Prénom</th>
+                    <th>Nom</th>
+                    <th>Age</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Benoit</td>
+                    <td>Bremaud</td>
+                    <td>42</td>
+                </tr>
+            </tbody>
+            <tbody>
+                <tr> <!-- <tr class="table-primary" > -->
+                    <td>Viktor</td>
+                    <td>Argirov</td>
+                    <td>23</td>
+                </tr>
+            </tbody>
+            <tbody>
+                <tr> <!-- <tr class="table-danger"> -->
+                    <td>Valeria</td>
+                    <td>Pallas</td>
+                    <td>38</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </body>
 </html>
