8bc1ff245b14139ad3ac63e8f62ca535a517256e
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-19 10:17:11 +0200

    feat: added align-items property in Flexbox with Bootstrap
    Implemented Flexbox layouts using the `align-items` property in Bootstrap.
    The code includes multiple container divs with different `align-items` values.
    Each container div contains three child divs with background colors and content.
    The `align-items-stretch` class is used to stretch the child divs to match the container's height, `align-items-start` to align them to the start, `align-items-center` to center them vertically, and `align-items-end` to align them to the end.
    Additionally, a combination of `align-items-end` and `justify-content-center` classes is used to align the child divs to the end vertically and center them horizontally.
    Lastly, there is an `align-items-lg-center` class used to align the child divs to the center vertically on larger screens.
    
    Refs: https://youtu.be/NgBk_-7k3hw

--- tutoriel/index.php
+++ tutoriel/index.php
@@ -4,43 +4,50 @@
     <!-- Required meta tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Flexbox avec bootstrap : justify-content</title>
+    <title>Flexbox avec bootstrap : align-items</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
     <hr>
-    <p>div style="height: 200px" class="container bg-dark d-flex justify-content-end"</p>
-    <div style="height: 200px" class="container bg-dark d-flex justify-content-end">
+    <p>div style="height: 200px" class="container bg-dark d-flex align-items-stretch"</p>
+    <div style="height: 200px" class="container bg-dark d-flex align-items-stretch">
         <div class="w-25 p-2 text-white bg-primary">1</div>
         <div class="w-25 p-2 text-white bg-warning">2</div>
         <div class="w-25 p-2 text-white bg-danger">3</div>
     </div>
     <hr>
-    <p>div style="height: 200px" class="container bg-dark d-flex justify-content-center"</p>
-    <div style="height: 200px" class="container bg-dark d-flex justify-content-center">
+    <p>div style="height: 200px" class="container bg-dark d-flex align-items-start"</p>
+    <div style="height: 200px" class="container bg-dark d-flex align-items-start">
         <div class="w-25 p-2 text-white bg-primary">1</div>
         <div class="w-25 p-2 text-white bg-warning">2</div>
         <div class="w-25 p-2 text-white bg-danger">3</div>
     </div>
     <hr>
-    <p>div style="height: 200px" class="container bg-dark d-flex justify-content-between"</p>
-    <div style="height: 200px" class="container bg-dark d-flex justify-content-between">
+    <p>div style="height: 200px" class="container bg-dark d-flex align-items-center"</p>
+    <div style="height: 200px" class="container bg-dark d-flex align-items-center">
         <div class="w-25 p-2 text-white bg-primary">1</div>
         <div class="w-25 p-2 text-white bg-warning">2</div>
         <div class="w-25 p-2 text-white bg-danger">3</div>
     </div>
     <hr>
-    <p>div style="height: 200px" class="container bg-dark d-flex justify-content-evenly"</p>
-    <div style="height: 200px" class="container bg-dark d-flex justify-content-evenly">
+    <p>div style="height: 200px" class="container bg-dark d-flex align-items-end"</p>
+    <div style="height: 200px" class="container bg-dark d-flex align-items-end">
         <div class="w-25 p-2 text-white bg-primary">1</div>
         <div class="w-25 p-2 text-white bg-warning">2</div>
         <div class="w-25 p-2 text-white bg-danger">3</div>
     </div>
     <hr>
-    <p>div style="height: 200px" class="container bg-dark d-flex justify-content-around"</p>
-    <div style="height: 200px" class="container bg-dark d-flex justify-content-around">
+    <p>div style="height: 200px" class="container bg-dark d-flex align-items-end justify-content-center"</p>
+    <div style="height: 200px" class="container bg-dark d-flex align-items-end justify-content-center">
+        <div class="w-25 p-2 text-white bg-primary">1</div>
+        <div class="w-25 p-2 text-white bg-warning">2</div>
+        <div class="w-25 p-2 text-white bg-danger">3</div>
+    </div>
+    <hr>
+    <p>div style="height: 200px" class="container bg-dark d-flex align-items-lg-center"</p>
+    <div style="height: 200px" class="container bg-dark d-flex align-items-lg-center">
         <div class="w-25 p-2 text-white bg-primary">1</div>
         <div class="w-25 p-2 text-white bg-warning">2</div>
         <div class="w-25 p-2 text-white bg-danger">3</div>
