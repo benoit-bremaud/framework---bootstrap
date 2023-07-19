9d138d7bd45d14b677bb8a44d748c67f5beb203e
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-19 13:33:34 +0200

    feat: added Bootstrap Navbar component
    Implemented Bootstrap Navbar component to create a navigation bar.
    
    - Created a responsive navbar using the `navbar` class and the `navbar-expand-md` class to enable responsive behavior.
    - Added a light background color to the navbar using the `bg-light` class.
    - Added a toggle button to collapse the menu on smaller screens using the `navbar-toggler` class and the `navbar-toggler-icon` class.
    - Implemented a collapsible menu using the `navbar-collapse` class and the `collapse` class. The menu contains four navigation links.
    
    Refs: https://youtu.be/XCoto7B7cuk

--- tutoriel/index.php
+++ tutoriel/index.php
@@ -4,65 +4,36 @@
     <!-- Required meta tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Le composant Nav</title>
+    <title>Le composant Navbar</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-
-    <header class="bg-warning">
+    <nav class="navbar navbar-expand-md navbar-light bg-light">
         <div class="container">
-            <nav class="nav">
-                <a href="#" class="nav-link">Lien 1</a>
-                <a href="#" class="nav-link">Lien 2</a>
-                <a href="#" class="nav-link">Lien 3</a>
-                <a href="#" class="nav-link disabled">Lien 4</a>
-            </nav>
-        </div>
-    </header>
-    <hr>
-    <div class="container">
-        <nav>
-            <ul class="nav justify-content-end">
-                <li><a href="#" class="nav-link">Lien 1</a></li>
-                <li><a href="#" class="nav-link">Lien 2</a></li>
-                <li><a href="#" class="nav-link">Lien 3</a></li>
-                <li><a href="#" class="nav-link">Lien 4</a></li>
-            </ul>
-        </nav>
-    </div>
-    <hr>
-    <div class="container">
-        <nav>
-            <ul class="nav flex-column">
-                <li><a href="#" class="nav-link">Lien 1</a></li>
-                <li><a href="#" class="nav-link">Lien 2</a></li>
-                <li><a href="#" class="nav-link">Lien 3</a></li>
-                <li><a href="#" class="nav-link">Lien 4</a></li>
-            </ul>
-        </nav>
-    </div>
-    <hr>
-    <div class="container">
-        <nav>
-            <ul class="nav">
-                <li><a href="#" class="nav-link">Lien 1</a></li>
-                <li><a href="#" class="nav-link">Lien 2</a></li>
-                <li class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Sous menu</a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#" class="dropdown-item">Sous menu 1</a></li>
-                        <li><a href="#" class="dropdown-item">Sous menu 2</a></li>
-                        <li><a href="#" class="dropdown-item">Sous menu 3</a></li>
-                    </ul>
-                </li>
-                <li><a href="#" class="nav-link">Lien 3</a></li>
-                <li><a href="#" class="nav-link">Lien 4</a></li>
-            </ul>
-        </nav>
-    </div>
+            <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="menu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="menu">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">A propos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Contact</a>
+                    </li>
+                </ul>
 
+            </div>
+        </div>
+    </nav>
 
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
