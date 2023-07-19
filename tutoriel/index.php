3e3abb1935688efa5a949dd11dde01c66f8e5293
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-19 10:23:50 +0200

    feat: added align-self property in Flexbox with Bootstrap
    Implemented Flexbox layouts using the `align-self` property in Bootstrap.
    The code includes container divs with child divs that have different `align-self` values.
    In the first container, the child divs have `align-self-start`, `align-self-end`, and `align-self-center` classes to align themselves vertically.
    In the second container, the child divs have `align-items-start` and additional `flex-grow-1` classes to control their growth.
    The first child div is aligned to the start, while the other two child divs have equal growth to fill the remaining space.
    
    Refs: https://youtu.be/NgBk_-7k3hw

--- tutoriel/index.php
+++ tutoriel/index.php
@@ -4,53 +4,25 @@
     <!-- Required meta tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Flexbox avec bootstrap : align-items</title>
+    <title>Flexbox avec bootstrap : align-self</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
     <hr>
-    <p>div style="height: 200px" class="container bg-dark d-flex align-items-stretch"</p>
-    <div style="height: 200px" class="container bg-dark d-flex align-items-stretch">
-        <div class="w-25 p-2 text-white bg-primary">1</div>
-        <div class="w-25 p-2 text-white bg-warning">2</div>
-        <div class="w-25 p-2 text-white bg-danger">3</div>
+    <p>div style="height: 200px" class="container bg-dark d-flex"</p>
+    <div style="height: 200px" class="container bg-dark d-flex">
+        <div class="w-25 p-2 text-white bg-primary align-self-start">1</div>
+        <div class="w-25 p-2 text-white bg-warning align-self-end">2</div>
+        <div class="w-25 p-2 text-white bg-danger align-self-center">3</div>
     </div>
     <hr>
     <p>div style="height: 200px" class="container bg-dark d-flex align-items-start"</p>
     <div style="height: 200px" class="container bg-dark d-flex align-items-start">
         <div class="w-25 p-2 text-white bg-primary">1</div>
-        <div class="w-25 p-2 text-white bg-warning">2</div>
-        <div class="w-25 p-2 text-white bg-danger">3</div>
-    </div>
-    <hr>
-    <p>div style="height: 200px" class="container bg-dark d-flex align-items-center"</p>
-    <div style="height: 200px" class="container bg-dark d-flex align-items-center">
-        <div class="w-25 p-2 text-white bg-primary">1</div>
-        <div class="w-25 p-2 text-white bg-warning">2</div>
-        <div class="w-25 p-2 text-white bg-danger">3</div>
-    </div>
-    <hr>
-    <p>div style="height: 200px" class="container bg-dark d-flex align-items-end"</p>
-    <div style="height: 200px" class="container bg-dark d-flex align-items-end">
-        <div class="w-25 p-2 text-white bg-primary">1</div>
-        <div class="w-25 p-2 text-white bg-warning">2</div>
-        <div class="w-25 p-2 text-white bg-danger">3</div>
-    </div>
-    <hr>
-    <p>div style="height: 200px" class="container bg-dark d-flex align-items-end justify-content-center"</p>
-    <div style="height: 200px" class="container bg-dark d-flex align-items-end justify-content-center">
-        <div class="w-25 p-2 text-white bg-primary">1</div>
-        <div class="w-25 p-2 text-white bg-warning">2</div>
-        <div class="w-25 p-2 text-white bg-danger">3</div>
-    </div>
-    <hr>
-    <p>div style="height: 200px" class="container bg-dark d-flex align-items-lg-center"</p>
-    <div style="height: 200px" class="container bg-dark d-flex align-items-lg-center">
-        <div class="w-25 p-2 text-white bg-primary">1</div>
-        <div class="w-25 p-2 text-white bg-warning">2</div>
-        <div class="w-25 p-2 text-white bg-danger">3</div>
+        <div class="w-25 p-2 text-white bg-warning flex-grow-1">2</div>
+        <div class="w-25 p-2 text-white bg-danger flex-grow-1">3</div>
     </div>
 </body>
 </html>
