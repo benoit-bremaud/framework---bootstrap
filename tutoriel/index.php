442ed5f1d64b793880d8653a40503cf1f5b18632
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-19 11:48:42 +0200

    feat: added Bootstrap Buttons component
    Implemented Bootstrap Buttons component with different color variations, sizes, and styles.
    The code includes primary, secondary, success, danger, warning, info, light, and dark buttons.
    It also showcases outline buttons using the `btn-outline-*` classes.
    The buttons have different sizes, including `btn-sm` and `btn-lg`.
    Disabled buttons are also included in the code.
    
    Refs: https://youtu.be/3qSikqkNJzA

--- tutoriel/index.php
+++ tutoriel/index.php
@@ -10,53 +10,57 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-    <hr>
-    <div class="container py-4">
-        <div class="alert alert-primary">Un message de couleur primary.</div>
-        <div class="alert alert-secondary">Un message de couleur secondary.</div>
-        <div class="alert alert-success">Un message de couleur success.</div>
-        <div class="alert alert-danger">Un message de couleur danger.</div>
-        <div class="alert alert-warning">Un message de couleur warning.</div>
+    <div class="container py-2">
+        <button class="btn btn-primary">Primary</button>
+        <button class="btn btn-secondary">Secondary</button>
+        <button class="btn btn-danger">Danger</button>
     </div>
     <hr>
-    <div class="container py-4">
-        <div class="alert alert-primary">Un message de couleur primary. <a href="#">Mettre un lien</a></div>
-        <div class="alert alert-secondary">Un message de couleur secondary. <a href="#">Mettre un lien</a></div>
-        <div class="alert alert-success">Un message de couleur success.</div>
-        <div class="alert alert-danger">Un message de couleur danger.</div>
-        <div class="alert alert-warning">Un message de couleur warning.</div>
+    <div class="container py-2">
+        <button class="btn btn-primary">Primary</button>
+        <button class="btn btn-secondary">Secondary</button>
+        <button class="btn btn-success">Success</button>
+        <button class="btn btn-danger">Danger</button>
+        <button class="btn btn-warning">Warning</button>
+        <button class="btn btn-info">Info</button>
+        <button class="btn btn-light">Light</button>
+        <button class="btn btn-dark ">Dark</button>
+        <hr>
+        <a href="#" class="btn btn-primary">Lien primary</a>
+        <hr>
+        <input class="btn btn-success" type="submit" value="Envoyer">
     </div>
     <hr>
-    <div class="container py-4">
-        <div class="alert alert-primary">Un message de couleur primary. <a href="#" class="alert-link">Mettre un lien</a></div>
-        <div class="alert alert-secondary">Un message de couleur secondary. <a href="#" class="alert-link">Mettre un lien</a></div>
-        <div class="alert alert-success">Un message de couleur success.</div>
-        <div class="alert alert-danger">Un message de couleur danger.</div>
-        <div class="alert alert-warning">Un message de couleur warning.</div>
+    <div class="container py-2">
+        <button class="btn btn-outline-primary">Primary</button>
+        <button class="btn btn-outline-secondary">Secondary</button>
+        <button class="btn btn-outline-success">Success</button>
+        <button class="btn btn-outline-danger">Danger</button>
+        <button class="btn btn-outline-warning">Warning</button>
+        <button class="btn btn-outline-info">Info</button>
+        <button class="btn btn-outline-light">Light</button>
+        <button class="btn btn-outline-dark ">Dark</button>
     </div>
     <hr>
-    <div class="container py-4">
-        <div class="alert alert-primary alert-dismissible">Un message de couleur primary.
-            <a href="#" class="alert-link">Mettre un lien</a>
-            <button class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-        <div class="alert alert-secondary alert-dismissible">Un message de couleur secondary. 
-            <a href="#" class="alert-link">Mettre un lien</a>
-            <button class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-        <div class="alert alert-success alert-dismissible">Un message de couleur success. 
-            <a href="#" class="alert-link">Mettre un lien</a>
-            <button class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-        <div class="alert alert-danger alert-dismissible">Un message de couleur danger. 
-            <a href="#" class="alert-link">Mettre un lien</a>
-            <button class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-        <div class="alert alert-warning alert-dismissible">Un message de couleur warning. 
-            <a href="#" class="alert-link">Mettre un lien</a>
-            <button class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
+    <div class="container py-2">
+        <button class="btn btn-outline-primary btn-sm">Primary</button>
+        <button class="btn btn-outline-secondary">Secondary</button>
+        <button class="btn btn-outline-success btn-lg">Success</button>
+        <button class="btn btn-outline-danger">Danger</button>
+        <button class="btn btn-outline-warning">Warning</button>
+        <button class="btn btn-outline-info">Info</button>
+        <button class="btn btn-outline-light">Light</button>
+        <button class="btn btn-outline-dark ">Dark</button>
     </div>
+    <hr>
+    <div class="container py-2">
+        <button class="btn btn-outline-primary btn-sm" disabled>Primary</button>
+        <button class="btn btn-outline-secondary">Secondary</button>
+        <hr>
+        <a class="btn btn-primary disabled" href="#">Bouton désactivé</a>
+    </div>
+
+    <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
