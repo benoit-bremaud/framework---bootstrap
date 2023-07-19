0a426a4fda4eab760236459ed20f25693384f3d7
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-19 09:13:16 +0200

    feat: added utility classes for shadows in Bootstrap
    Implemented utility classes to apply different shadow effects in Bootstrap. The code includes three columns within a container, each with different shadow classes applied: "shadow" for a regular shadow effect, "shadow-sm" for a small shadow, and "shadow-lg" for a large shadow. The content within each column is displayed with a heading and a paragraph.
    
    Refs: #123

--- tutoriel/index.php
+++ tutoriel/index.php
@@ -4,23 +4,27 @@
     <!-- Required meta tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Les classes utilitaires : la propriété display</title>
+    <title>Les classes utilitaires : les ombres</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container">
-        <p class="bg-primary text-white p-2">Un premier paragraphe</p>
-        <p class="bg-danger text-white p-2 d-inline">Un deuxième paragraphe</p>
-        <hr>
-        <a class="bg-warning p-2 d-block"href="#">Lien</a>
-    </div>
-    <br>
-    <div class="container">
-        <p class="bg-primary text-white p-2 d-sm-none">Affichage sur smartphone</p>
-        <p class="bg-success text-white p-2 d-md-none">Affichage sur tablette</p>
-        <p class="bg-danger text-white p-2 d-none d-lg-block">Affichage sur grand ecran</p>
+    <div class="container p-5">
+        <div class="row">
+            <div class="col mx-3 shadow">
+                <h4>class="col mx-3 shadow"</h4>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore, sapiente.</p>
+            </div>
+            <div class="col mx-3 shadow-sm">
+                <h4>class="col mx-3 shadow-sm"</h4>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore, sapiente.</p>
+            </div>
+            <div class="col mx-3 shadow-lg">
+                <h4>class="col mx-3 shadow-lg"</h4>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore, sapiente.</p>
+            </div>
+        </div>
     </div>
 </body>
 </html>
