785aceb229c7f0808189bba41e39735f497763db
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-19 09:58:52 +0200

    feat: added Flexbox with flex-row in Bootstrap
    Implemented Flexbox layout using the `flex-row` class in Bootstrap.
    The code includes three div elements within a container, all aligned horizontally.
    The divs have different background colors and content.
    Additionally, there are variations with the `w-50` class to control the width of the divs, and the `flex-wrap` class to allow wrapping of divs when needed.
    
    Refs: #123

--- tutoriel/index.php
+++ tutoriel/index.php
@@ -4,25 +4,28 @@
     <!-- Required meta tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Les classes utilitaires : les dimensions</title>
+    <title>Flexbox avec bootstrap : flex-row</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container p-5">
-        <p class="bg-primary p-2 test-white w-25">Largeur de 25%</p>
-        <p class="bg-success p-2 test-white w-50">Largeur de 50%</p>
-        <p class="bg-danger p-2 test-white w-75">Largeur de 75%</p>
-        <p class="bg-secondary p-2 test-white w-100">Largeur de 100%</p>
-        <p class="bg-warning p-2 test-white w-auto">Largeur de auto</p>
+    <div style="height: 200px" class="container bg-dark d-flex">
+        <div class="p-2 text-white bg-primary">1</div>
+        <div class="p-2 text-white bg-warning">2</div>
+        <div class="p-2 text-white bg-danger">3</div>
     </div>
-    <div class="container border mt-5" style="height: 200px">
-        <p class="bg-primary p-2 test-white d-inline-block h-25">Hauteur de 25%</p>
-        <p class="bg-success p-2 test-white d-inline-block h-50">Hauteur de 50%</p>
-        <p class="bg-danger p-2 test-white d-inline-block h-75">Hauteur de 75%</p>
-        <p class="bg-secondary p-2 test-white d-inline-block h-100">Hauteur de 100%</p>
-        <p class="bg-warning p-2 test-white d-inline-block h-auto">Hauteur de auto</p>
+    <hr>
+    <div style="height: 200px" class="container bg-dark d-flex">
+        <div class="w-50 p-2 text-white bg-primary">1</div>
+        <div class="w-50 p-2 text-white bg-warning">2</div>
+        <div class="w-50 p-2 text-white bg-danger">3</div>
+    </div>
+    <hr>
+    <div style="height: 200px" class="container bg-dark d-flex flex-wrap">
+        <div class="w-50 p-2 text-white bg-primary">1</div>
+        <div class="w-50 p-2 text-white bg-warning">2</div>
+        <div class="w-50 p-2 text-white bg-danger">3</div>
     </div>
 </body>
 </html>
