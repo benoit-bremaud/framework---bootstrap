400816bbbf0332f093f17810fddd9ef7f1c2a21d
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-19 09:05:48 +0200

    feat: added utility classes for the display property in Bootstrap
    Implemented utility classes to control the display property in Bootstrap. The code includes paragraphs and a link with different display classes applied. The first paragraph is displayed as a block element, the second paragraph is displayed inline, and the link is displayed as a block-level element. Additionally, there are paragraphs with responsive display classes to control their visibility on different screen sizes.
    
    Refs: #123

--- tutoriel/index.php
+++ tutoriel/index.php
@@ -4,23 +4,23 @@
     <!-- Required meta tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Les classes utilitaires : les bordures</title>
+    <title>Les classes utilitaires : la propriété display</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
     <div class="container">
-        <div class="row py-3"> <!-- Padding Top and Bottom of 3 -->
-            <div class="col">
-                <!-- Ajout d'une bordure sur le premier paragraphe -->
-                <p class="border border-2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos quas libero et modi aspernatur est repudiandae assumenda. Quod at consequuntur voluptatibus hic aspernatur aut ad provident magni exercitationem nesciunt a tenetur ducimus ullam, harum nobis impedit! Beatae magni omnis necessitatibus incidunt mollitia deserunt iusto asperiores labore rerum deleniti! Laborum, pariatur!</p>
-            </div>
-            <div class="col">
-                <!-- AUtre bordure sur paragraphe 2 -->
-                <p class="border border-primary border-2 rounded-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores harum optio debitis dolorem repellendus nisi eos. Incidunt nobis enim totam nesciunt! Pariatur blanditiis cupiditate nihil, consectetur ipsa explicabo voluptas alias repellat omnis nam ea dolorum laborum iure harum a, asperiores porro unde, ut esse corporis maxime suscipit! Eligendi, rem tempore?</p>
-            </div>
-        </div>
+        <p class="bg-primary text-white p-2">Un premier paragraphe</p>
+        <p class="bg-danger text-white p-2 d-inline">Un deuxième paragraphe</p>
+        <hr>
+        <a class="bg-warning p-2 d-block"href="#">Lien</a>
+    </div>
+    <br>
+    <div class="container">
+        <p class="bg-primary text-white p-2 d-sm-none">Affichage sur smartphone</p>
+        <p class="bg-success text-white p-2 d-md-none">Affichage sur tablette</p>
+        <p class="bg-danger text-white p-2 d-none d-lg-block">Affichage sur grand ecran</p>
     </div>
 </body>
 </html>
