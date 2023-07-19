1725ee7588510058a9a12eadc7bfe09d2a3cb025
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-19 09:49:48 +0200

    feat: added utility classes for dimensions in Bootstrap
    Implemented utility classes to control the dimensions (width and height) of elements in Bootstrap.
    The code includes paragraphs with different width classes applied, such as "w-25" (25% width), "w-50" (50% width), "w-75" (75% width), "w-100" (100% width), and "w-auto" (auto width).
    Similarly, there are paragraphs with different height classes applied, such as "h-25" (25% height), "h-50" (50% height), "h-75" (75% height), "h-100" (100% height), and "h-auto" (auto height).
    
    Refs: #123

--- tutoriel/index.php
+++ tutoriel/index.php
@@ -4,27 +4,25 @@
     <!-- Required meta tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Les classes utilitaires : les ombres</title>
+    <title>Les classes utilitaires : les dimensions</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
     <div class="container p-5">
-        <div class="row">
-            <div class="col mx-3 shadow">
-                <h4>class="col mx-3 shadow"</h4>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore, sapiente.</p>
-            </div>
-            <div class="col mx-3 shadow-sm">
-                <h4>class="col mx-3 shadow-sm"</h4>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore, sapiente.</p>
-            </div>
-            <div class="col mx-3 shadow-lg">
-                <h4>class="col mx-3 shadow-lg"</h4>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore, sapiente.</p>
-            </div>
-        </div>
+        <p class="bg-primary p-2 test-white w-25">Largeur de 25%</p>
+        <p class="bg-success p-2 test-white w-50">Largeur de 50%</p>
+        <p class="bg-danger p-2 test-white w-75">Largeur de 75%</p>
+        <p class="bg-secondary p-2 test-white w-100">Largeur de 100%</p>
+        <p class="bg-warning p-2 test-white w-auto">Largeur de auto</p>
+    </div>
+    <div class="container border mt-5" style="height: 200px">
+        <p class="bg-primary p-2 test-white d-inline-block h-25">Hauteur de 25%</p>
+        <p class="bg-success p-2 test-white d-inline-block h-50">Hauteur de 50%</p>
+        <p class="bg-danger p-2 test-white d-inline-block h-75">Hauteur de 75%</p>
+        <p class="bg-secondary p-2 test-white d-inline-block h-100">Hauteur de 100%</p>
+        <p class="bg-warning p-2 test-white d-inline-block h-auto">Hauteur de auto</p>
     </div>
 </body>
 </html>
