bc7d623859eebc4dfc0b6fa30301bbdfcab6ff34
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-19 10:08:42 +0200

    feat: added justify-content property in Flexbox with Bootstrap
    Implemented Flexbox layouts using the `justify-content` property in Bootstrap.
    The code includes multiple container divs with different `justify-content` values.
    Each container div contains three child divs with background colors and content.
    The `justify-content-end` class is used to align the child divs to the end, `justify-content-center` to center them, `justify-content-between` to evenly distribute them with space between, `justify-content-evenly` to evenly distribute them with equal space around, and `justify-content-around` to evenly distribute them with equal space around but half the space at the ends.
    
    Refs: https://youtu.be/NgBk_-7k3hw

--- tutoriel/index.php
+++ tutoriel/index.php
@@ -4,28 +4,46 @@
     <!-- Required meta tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Flexbox avec bootstrap : flex-row</title>
+    <title>Flexbox avec bootstrap : justify-content</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-    <div style="height: 200px" class="container bg-dark d-flex">
-        <div class="p-2 text-white bg-primary">1</div>
-        <div class="p-2 text-white bg-warning">2</div>
-        <div class="p-2 text-white bg-danger">3</div>
+    <hr>
+    <p>div style="height: 200px" class="container bg-dark d-flex justify-content-end"</p>
+    <div style="height: 200px" class="container bg-dark d-flex justify-content-end">
+        <div class="w-25 p-2 text-white bg-primary">1</div>
+        <div class="w-25 p-2 text-white bg-warning">2</div>
+        <div class="w-25 p-2 text-white bg-danger">3</div>
     </div>
     <hr>
-    <div style="height: 200px" class="container bg-dark d-flex">
-        <div class="w-50 p-2 text-white bg-primary">1</div>
-        <div class="w-50 p-2 text-white bg-warning">2</div>
-        <div class="w-50 p-2 text-white bg-danger">3</div>
+    <p>div style="height: 200px" class="container bg-dark d-flex justify-content-center"</p>
+    <div style="height: 200px" class="container bg-dark d-flex justify-content-center">
+        <div class="w-25 p-2 text-white bg-primary">1</div>
+        <div class="w-25 p-2 text-white bg-warning">2</div>
+        <div class="w-25 p-2 text-white bg-danger">3</div>
     </div>
     <hr>
-    <div style="height: 200px" class="container bg-dark d-flex flex-wrap">
-        <div class="w-50 p-2 text-white bg-primary">1</div>
-        <div class="w-50 p-2 text-white bg-warning">2</div>
-        <div class="w-50 p-2 text-white bg-danger">3</div>
+    <p>div style="height: 200px" class="container bg-dark d-flex justify-content-between"</p>
+    <div style="height: 200px" class="container bg-dark d-flex justify-content-between">
+        <div class="w-25 p-2 text-white bg-primary">1</div>
+        <div class="w-25 p-2 text-white bg-warning">2</div>
+        <div class="w-25 p-2 text-white bg-danger">3</div>
+    </div>
+    <hr>
+    <p>div style="height: 200px" class="container bg-dark d-flex justify-content-evenly"</p>
+    <div style="height: 200px" class="container bg-dark d-flex justify-content-evenly">
+        <div class="w-25 p-2 text-white bg-primary">1</div>
+        <div class="w-25 p-2 text-white bg-warning">2</div>
+        <div class="w-25 p-2 text-white bg-danger">3</div>
+    </div>
+    <hr>
+    <p>div style="height: 200px" class="container bg-dark d-flex justify-content-around"</p>
+    <div style="height: 200px" class="container bg-dark d-flex justify-content-around">
+        <div class="w-25 p-2 text-white bg-primary">1</div>
+        <div class="w-25 p-2 text-white bg-warning">2</div>
+        <div class="w-25 p-2 text-white bg-danger">3</div>
     </div>
 </body>
 </html>
