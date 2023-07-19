c22969fa50e442dc0387f723d4137fcf560e0982
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-19 12:23:52 +0200

    feat: added Bootstrap Nav component
    Implemented Bootstrap Nav component to create navigation menus.
    
    - Added a navigation bar with links using the `nav` and `nav-link` classes. The navigation bar has a yellow background color.
    
    - Created a horizontal menu using an unordered list and the `justify-content-end` class to align the items to the right.
    
    - Created a vertical menu using an unordered list and the `flex-column` class to stack the items vertically.
    
    - Implemented a dropdown menu within the navigation bar using the `dropdown` class and the `dropdown-toggle` class for the parent item.
    
    The dropdown menu contains three submenu links.
    
    Refs: https://youtu.be/YX614dRtPKY

--- tutoriel/index.php
+++ tutoriel/index.php
@@ -4,33 +4,65 @@
     <!-- Required meta tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Le composant Carousel</title>
+    <title>Le composant Nav</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container">
-        <div id="diaporama" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="images/image_1.jpeg" alt="" class="d-block w-100">
-                </div>
-                <div class="carousel-item">
-                    <img src="images/image_2.jpeg" alt="" class="d-block w-100">
-                </div>
-                <div class="carousel-item">
-                    <img src="images/image_3.jpeg" alt="" class="d-block w-100">
-                </div>
-            </div>
-            <button class="carousel-control-prev"type="button" data-bs-target="#diaporama" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon"></span>
-            </button>
-            <button class="carousel-control-next"type="button" data-bs-target="#diaporama" data-bs-slide="next">
-                <span class="carousel-control-next-icon"></span>
-            </button>
+
+    <header class="bg-warning">
+        <div class="container">
+            <nav class="nav">
+                <a href="#" class="nav-link">Lien 1</a>
+                <a href="#" class="nav-link">Lien 2</a>
+                <a href="#" class="nav-link">Lien 3</a>
+                <a href="#" class="nav-link disabled">Lien 4</a>
+            </nav>
         </div>
+    </header>
+    <hr>
+    <div class="container">
+        <nav>
+            <ul class="nav justify-content-end">
+                <li><a href="#" class="nav-link">Lien 1</a></li>
+                <li><a href="#" class="nav-link">Lien 2</a></li>
+                <li><a href="#" class="nav-link">Lien 3</a></li>
+                <li><a href="#" class="nav-link">Lien 4</a></li>
+            </ul>
+        </nav>
     </div>
+    <hr>
+    <div class="container">
+        <nav>
+            <ul class="nav flex-column">
+                <li><a href="#" class="nav-link">Lien 1</a></li>
+                <li><a href="#" class="nav-link">Lien 2</a></li>
+                <li><a href="#" class="nav-link">Lien 3</a></li>
+                <li><a href="#" class="nav-link">Lien 4</a></li>
+            </ul>
+        </nav>
+    </div>
+    <hr>
+    <div class="container">
+        <nav>
+            <ul class="nav">
+                <li><a href="#" class="nav-link">Lien 1</a></li>
+                <li><a href="#" class="nav-link">Lien 2</a></li>
+                <li class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Sous menu</a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#" class="dropdown-item">Sous menu 1</a></li>
+                        <li><a href="#" class="dropdown-item">Sous menu 2</a></li>
+                        <li><a href="#" class="dropdown-item">Sous menu 3</a></li>
+                    </ul>
+                </li>
+                <li><a href="#" class="nav-link">Lien 3</a></li>
+                <li><a href="#" class="nav-link">Lien 4</a></li>
+            </ul>
+        </nav>
+    </div>
+
 
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
