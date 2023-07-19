a1f06e382a4528e11e9abed38017531cf64ac8ec
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-19 12:04:56 +0200

    feat: added Bootstrap Carousel component
    Implemented Bootstrap Carousel component with a slideshow of images.
    
    The carousel is configured with three slides that display images in a responsive manner.
    
    It includes previous and next navigation buttons using the `carousel-control-prev` and `carousel-control-next` classes.
    
    The carousel is set to automatically cycle through the slides.
    
    Images used in the carousel: image_1.jpeg, image_2.jpeg, image_3.jpeg.
    
    Refs: https://youtu.be/ZnqNp9VCtko

--- tutoriel/index.php
+++ tutoriel/index.php
@@ -4,60 +4,32 @@
     <!-- Required meta tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Le composant Alerts</title>
+    <title>Le composant Carousel</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container py-2">
-        <button class="btn btn-primary">Primary</button>
-        <button class="btn btn-secondary">Secondary</button>
-        <button class="btn btn-danger">Danger</button>
-    </div>
-    <hr>
-    <div class="container py-2">
-        <button class="btn btn-primary">Primary</button>
-        <button class="btn btn-secondary">Secondary</button>
-        <button class="btn btn-success">Success</button>
-        <button class="btn btn-danger">Danger</button>
-        <button class="btn btn-warning">Warning</button>
-        <button class="btn btn-info">Info</button>
-        <button class="btn btn-light">Light</button>
-        <button class="btn btn-dark ">Dark</button>
-        <hr>
-        <a href="#" class="btn btn-primary">Lien primary</a>
-        <hr>
-        <input class="btn btn-success" type="submit" value="Envoyer">
-    </div>
-    <hr>
-    <div class="container py-2">
-        <button class="btn btn-outline-primary">Primary</button>
-        <button class="btn btn-outline-secondary">Secondary</button>
-        <button class="btn btn-outline-success">Success</button>
-        <button class="btn btn-outline-danger">Danger</button>
-        <button class="btn btn-outline-warning">Warning</button>
-        <button class="btn btn-outline-info">Info</button>
-        <button class="btn btn-outline-light">Light</button>
-        <button class="btn btn-outline-dark ">Dark</button>
-    </div>
-    <hr>
-    <div class="container py-2">
-        <button class="btn btn-outline-primary btn-sm">Primary</button>
-        <button class="btn btn-outline-secondary">Secondary</button>
-        <button class="btn btn-outline-success btn-lg">Success</button>
-        <button class="btn btn-outline-danger">Danger</button>
-        <button class="btn btn-outline-warning">Warning</button>
-        <button class="btn btn-outline-info">Info</button>
-        <button class="btn btn-outline-light">Light</button>
-        <button class="btn btn-outline-dark ">Dark</button>
-    </div>
-    <hr>
-    <div class="container py-2">
-        <button class="btn btn-outline-primary btn-sm" disabled>Primary</button>
-        <button class="btn btn-outline-secondary">Secondary</button>
-        <hr>
-        <a class="btn btn-primary disabled" href="#">Bouton désactivé</a>
+    <div class="container">
+        <div id="diaporama" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="images/image_1.jpeg" alt="" class="d-block w-100">
+                </div>
+                <div class="carousel-item">
+                    <img src="images/image_2.jpeg" alt="" class="d-block w-100">
+                </div>
+                <div class="carousel-item">
+                    <img src="images/image_3.jpeg" alt="" class="d-block w-100">
+                </div>
+            </div>
+            <button class="carousel-control-prev"type="button" data-bs-target="#diaporama" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon"></span>
+            </button>
+            <button class="carousel-control-next"type="button" data-bs-target="#diaporama" data-bs-slide="next">
+                <span class="carousel-control-next-icon"></span>
+            </button>
+        </div>
     </div>
 
     <!-- Bootstrap JS -->
