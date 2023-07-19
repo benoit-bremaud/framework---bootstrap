39694baecb8d8dd5cd57ff62b75e67312ed3b792
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-19 13:56:34 +0200

    feat: added Bootstrap Navbar component
    Implemented Bootstrap Navbar component to create a navigation bar.
    
    - Created a responsive navbar using the `navbar` class and the `navbar-expand-md` class to enable responsive behavior.
    - Added a light background color to the navbar using the `bg-light` class.
    - Added a toggle button to collapse the menu on smaller screens using the `navbar-toggler` class and the `navbar-toggler-icon` class.
    - Implemented a collapsible menu using the `navbar-collapse` class and the `collapse` class.
    
    The menu contains four navigation links.
    
    Refs: https://youtu.be/7lVATDiqhjo

--- tutoriel/index.php
+++ tutoriel/index.php
@@ -4,38 +4,53 @@
     <!-- Required meta tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Le composant Navbar</title>
+    <title>Les formulaires</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-    <nav class="navbar navbar-expand-md navbar-light bg-light">
-        <div class="container">
-            <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="menu">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="menu">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">Accueil</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">Blog</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">A propos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">Contact</a>
-                    </li>
-                </ul>
-
+    <div class="container py-3">
+        <!-- <form action="">
+            <div class="mb-3">
+                <label class="form-label" for="name">Nom</label>
+                <input class="form-control form-control-sm"type="text" name="name" id="name" disabled>
             </div>
-        </div>
-    </nav>
+            <div class="mb-3">
+                <label class="form-label" for="email">Email</label>
+                <input class="form-control"type="text" name="email" id="email">
+            </div>
+            <div class="mb-3">
+                <label class="form-label" for="message">Message</label>
+                <textarea class="form-control"name="message" id="message" cols="30" rows="10"></textarea>
+            </div>
+        </form> -->
 
-    <!-- Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <form action="" class="row g-3">
+            <div class="col-md-6">
+                <label class="form-label" for="name">Nom</label>
+                <input class="form-control" type="text" name="name" id="name">
+            </div>
+            <div class="col-md-6">
+                <label class="form-label" for="email">Email</label>
+                <input class="form-control" type="email" name="email" id="email">
+            </div>
+            <div class="col-md-12">
+                <label class="form-label" for="adresse">Adresse</label>
+                <input class="form-control" type="text" name="adresse" id="adresse">
+            </div>
+            <div class="col-md-4">
+                <label class="form-label" for="cp">Code postal</label>
+                <input class="form-control" type="text" name="cp" id="cp">
+            </div>
+            <div class="col-md-8">
+                <label class="form-label" for="email">Ville</label>
+                <input class="form-control" type="text" name="ville" id="ville">
+            </div>
+            <div class="col-md-12">
+                <button class="btn btn-primary" type="submit">Envoyer</button>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
